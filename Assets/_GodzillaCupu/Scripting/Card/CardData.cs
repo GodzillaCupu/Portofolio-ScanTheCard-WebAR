ee120c1cd90e4010534db0ba231d8f1a0e0178d5
GodzillaCupu <prasetyo.djufri@gmail.com>
2025-07-17 02:37:44 +0700

    create and change scirpting

--- Assets/_GodzillaCupu/Scripting/Card/CardData.cs
+++ Assets/_GodzillaCupu/Scripting/Card/CardData.cs
@@ -4,46 +4,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CardData", menuName = "Scriptable Objects/Cards/Data")]
 public class CardData : ScriptableObject
 {
-    [SerializeField] private string cardName;
-    [SerializeField] private float tottalLosses;
-    [SerializeField] private int cardRank;
-    [SerializeField] private ImageData cardIcon;
-    [SerializeField] private ImageData cardBackground;
-    [SerializeField] private ImageData cardBorder;
+    [SerializeField] private string ID;
+    [SerializeField] private float lossAmount;
+    [SerializeField] private int rank;
+    [SerializeField] private Sprite icon;
+    [SerializeField] private Sprite artworkBackground;
+    [SerializeField] private Sprite borderBackground;
 
-    public int GetCardRank() => cardRank;
-    public string GetCardName() => cardName.ToUpper();
-    public string GetCardTottalLosses() => $"Rp. {tottalLosses} TRILLION";
-    public string GetYear() => $"2025 EDITION #{cardRank}";
+    //Text Data
+    public int GetCardRank() => rank;
+    public string GetCardName() => ID.ToUpper();
+    public string GetCardTottalLosses() => $"Rp. {lossAmount} TRILLION";
+    public string GetYear() => $"2025 EDITION #{rank}";
 
-    public Texture2D GetIconTexture() => cardIcon.GetTextureData();
-    public Sprite GetIconSprite() => cardIcon.GetSpriteData();
-    public Texture2D GetBackgroundTexture() => cardBackground.GetTextureData();
-    public Sprite GetBackgroundSprite() => cardBackground.GetSpriteData();
-    public Texture2D GetBorderTexture() => cardBorder.GetTextureData();
-    public Sprite GetBorderSprite() => cardBorder.GetSpriteData();
-
-    [Serializable]
-    public class ImageData
-    {
-        [SerializeField] private Sprite cardSpirite;
-        [SerializeField] private Texture2D cardImage;
-
-        public void GetImageData(Sprite _cardSprite, Texture2D _cardImage)
-        {
-            if (_cardImage == null || _cardSprite != null)
-            {
-                _cardImage = cardImage;
-                _cardSprite = cardSpirite;
-            }
-
-            cardImage = cardImage == null ? cardSpirite.texture : GetTextureData();
-            cardSpirite = cardSpirite == null ? Sprite.Create(cardImage, new Rect(0, 0, cardImage.width, cardImage.height), new Vector2(0.5f, 0.5f)) : GetSpriteData();
-        }
-
-        public Sprite GetSpriteData() => cardSpirite;
-
-        public Texture2D GetTextureData() => cardImage;
-    }
+    //Image Data
+    public Sprite GetIcon() => icon;
+    public Sprite GetBorderBackground() => borderBackground
+    ;
+    public Sprite GetArtworkBackground() => artworkBackground;
 
 }
